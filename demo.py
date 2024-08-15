fa10e560e27e0b42f532525e50a3ad40eeb672cc
C-Freeze <afreeze.carter@gmail.com>
2024-08-15 00:19:21 -0500

    init

---
+++ demo.py
@@ -0,0 +1,12 @@
+import lib_func
+
+def main():
+    x = 10
+    y = 5
+    print(f"{x} + {y} = {lib_func.add(x, y)}")
+    print(f"{x} - {y} = {lib_func.subtract(x, y)}")
+    # print(f"{x} * {y} = {lib_func.mul(x, y)}") #TODO: Add this
+    # print(f"{x} / {y} = {lib_func.div(x, y)}") #TODO: Add this
+    
+if __name__ == "__main__":
+    main()
